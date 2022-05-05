6284a0623818a6a1205dd70c1be6e9bd3facac7f
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-05-05 22:36:10 +0200

    docker improvements

--- run_chrome.sh
+++ run_chrome.sh
@@ -5,9 +5,10 @@ docker network create chrome-net
 # https://github.com/SeleniumHQ/docker-selenium
 # http://localhost:7900
 docker run -d --name chrome \
-  --net=chrome-net \
+  --net chrome-net \
   -p 4444:4444 \
   -p 7900:7900 \
   -e VNC_NO_PASSWORD=1 \
   --shm-size="2g" \
   seleniarm/standalone-chromium:101.0
+  #selenium/standalone-chrome:4.1.4-20220427
