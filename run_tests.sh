9f9138a78b6c78b4e48e1e1206822d751dec9122
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-05-10 22:00:47 +0200

    docker improvements

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,8 @@
 docker run --rm \
   --net=chrome-net \
   -e BROWSER_URL=http://chrome:4444 \
+  -e AUTOMATION_RUN_CI=true \
+  -e TEST_RAIL_TEST_RUN="${TEST_RAIL_TEST_RUN}" \
   -v $PWD:/home/gradle/autotests \
   -w /home/gradle/autotests \
   --entrypoint ./run_tests_entrypoint.sh \
