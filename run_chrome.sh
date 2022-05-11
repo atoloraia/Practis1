72bb691568dc60d6603be2f142cec4a9b44e36b2
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-05-11 15:57:51 +0200

    docker improvements

--- run_chrome.sh
+++ run_chrome.sh
@@ -2,6 +2,20 @@
 
 sh stop_chrome.sh
 docker network create chrome-net
+
+DOCKER_IMAGE="";
+
+case $(uname -m) in
+    arm64)
+      DOCKER_IMAGE="seleniarm/standalone-chromium:101.0"
+      ;;
+    *)
+      DOCKER_IMAGE="selenium/standalone-chrome:4.1.4-20220427"
+      ;;
+esac
+
+echo "Starting container from image $DOCKER_IMAGE"
+
 # https://github.com/SeleniumHQ/docker-selenium
 # http://localhost:7900
 docker run -d --name chrome \
@@ -10,5 +24,4 @@ docker run -d --name chrome \
   -p 7900:7900 \
   -e VNC_NO_PASSWORD=1 \
   --shm-size="2g" \
-  selenium/standalone-chrome:4.1.4-20220427
-  #seleniarm/standalone-chromium:101.0
+  $DOCKER_IMAGE
