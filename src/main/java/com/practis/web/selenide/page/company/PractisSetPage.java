1f2e7f5ad101a83c18b6538f05abcb00c328e0ec
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-05-30 16:59:27 +0200

    Enable TestRail

--- src/main/java/com/practis/web/selenide/page/company/PractisSetPage.java
+++ src/main/java/com/practis/web/selenide/page/company/PractisSetPage.java
@@ -1,23 +1,9 @@
 package com.practis.web.selenide.page.company;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.practis.utils.AwaitUtils.awaitElementCollectionSize;
-import static com.practis.utils.AwaitUtils.awaitElementEnabled;
-import static com.practis.web.selenide.configuration.ComponentObjectFactory.labelChallenge;
-import static com.practis.web.selenide.configuration.ComponentObjectFactory.snackbar;
-import static com.practis.web.util.SelenideSetDivUtilUtil.setDivText;
-import static org.openqa.selenium.support.How.CSS;
 
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import com.practis.dto.NewPractisSetInput;
-import com.practis.dto.NewScenarioInput;
-import java.util.List;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class PractisSetPage {
 
@@ -35,7 +21,5 @@ public class PractisSetPage {
   public void fillForm(final NewPractisSetInput inputData, final String label) {
     fillTitle(inputData);
     descriptionField.append(inputData.getDescription());
-
   }
-
 }
