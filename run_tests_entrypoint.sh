fb1c79e1826ab14e64912330cda0c210806f8eeb
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-06-05 15:28:51 +0200

    test run partially

--- run_tests_entrypoint.sh
+++ run_tests_entrypoint.sh
@@ -2,10 +2,4 @@
 
 gradle test --no-watch-fs --info \
   --tests com.practis.selenide.LoginTest \
-  --tests com.practis.selenide.admin.NewAdminTest \
-  --tests com.practis.selenide.admin.NewCompanyTest \
-  --tests com.practis.selenide.company.NewLabelTest \
-  --tests com.practis.selenide.company.NewTeamTest \
-  --tests com.practis.selenide.company.NewChallengeTest \
-  --tests com.practis.selenide.company.NewScenarioTest \
-  --tests com.practis.selenide.company.NewPractisSetTest
+  --tests com.practis.selenide.company.NewChallengeTest
