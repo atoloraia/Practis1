2010fdd8b7ccbf59bb25d9e67fe936984358efee
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-08-08 23:46:10 +0200

    revert ci

--- src/main/java/com/practis/rest/configuration/PractisClientConfiguration.java
+++ src/main/java/com/practis/rest/configuration/PractisClientConfiguration.java
@@ -2,7 +2,6 @@ package com.practis.rest.configuration;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static com.practis.rest.service.PractisApiService.getToken;
-import static com.practis.rest.service.PractisApiService.resetToken;
 import static com.practis.web.selenide.configuration.model.WebRestConfiguration.webRestConfig;
 import static java.lang.String.format;
 import static java.util.Objects.isNull;
@@ -12,10 +11,6 @@ import com.practis.rest.client.PractisApiClient;
 import feign.Feign;
 import feign.Logger.Level;
 import feign.RequestInterceptor;
-import feign.Response;
-import feign.RetryableException;
-import feign.Retryer.Default;
-import feign.codec.ErrorDecoder;
 import feign.form.FormEncoder;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
@@ -40,31 +35,9 @@ public class PractisClientConfiguration {
         .decoder(new JacksonDecoder(objectMapper()))
         .encoder(new FormEncoder(new JacksonEncoder(objectMapper())))
         .requestInterceptor(headerInterceptor())
-        .retryer(tokenRetryer())
-        .errorDecoder(new ErrorDecoder.Default() {
-          @Override
-          public Exception decode(final String methodKey, final Response response) {
-            if (response.status() == 401) {
-              throw new PractisAuthorizationException(response);
-            }
-            return super.decode(methodKey, response);
-          }
-        })
         .logger(new Slf4jLogger(PractisApiClient.class))
         .logLevel(Level.FULL)
         .target(PractisApiClient.class, webRestConfig().getPractisApiUrl());
-  }
-
-  private static Default tokenRetryer() {
-    return new Default() {
-      @Override
-      public void continueOrPropagate(final RetryableException e) {
-        if (e instanceof PractisAuthorizationException) {
-          resetToken();
-        }
-        super.continueOrPropagate(e);
-      }
-    };
   }
 
   private static RequestInterceptor headerInterceptor() {
