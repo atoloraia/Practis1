e35f0fa1cb4dfe21823cd2d841fada9b6d895475
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-09-20 18:20:40 +0200

    Add attributes DEV-9621

--- src/main/java/com/practis/web/selenide/component/user/invite/SaveAsDraftPopUp.java
+++ src/main/java/com/practis/web/selenide/component/user/invite/SaveAsDraftPopUp.java
@@ -11,7 +11,7 @@ public class SaveAsDraftPopUp {
   private final SelenideElement saveAsDraftTitle = $("div[data-test='save-as-draft-title']");
   private final SelenideElement saveAsDraftText = $("div[data-test='save-as-draft-description']");
   private final SelenideElement draftTitleField = $("input[data-test='save-as-draft-input']");
-  private final SelenideElement draftTitleErrorText = $(".sc-bixyNo.kqdtdT");
+  private final SelenideElement draftTitleErrorText = $("div[data-test='save-as-draft-input-error']");
   private final SelenideElement cancelButton = $("button[data-test='save-as-draft-cancel']");
   private final SelenideElement saveButton = $("button[data-test='save-as-draft-save']");
 }
