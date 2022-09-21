2f84d05b9941955f7b5d09b393826921b52f79ca
Anastasiya Halubovich <riikkimarley@gmail.com>
2022-09-21 10:48:09 +0200

    Add attributes DEV-9678

--- src/main/java/com/practis/web/selenide/page/admin/navigation/AdminPage.java
+++ src/main/java/com/practis/web/selenide/page/admin/navigation/AdminPage.java
@@ -1,7 +1,9 @@
 package com.practis.web.selenide.page.admin.navigation;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
 
@@ -13,4 +15,31 @@ public class AdminPage {
   private final SelenideElement searchField = $("input[data-test='table-search-input']");
   private final SelenideElement searchFieldIcon = $("div[data-test='table-search-input-icon']");
 
+  private final SelenideElement updatedTimestampText = $("span[data-test='table-timestamp-label']");
+  private final SelenideElement updatedTimestampButton = $("button[data-test='table-timestamp-refresh']");
+
+  private final SelenideElement paginationCounterText = $("div[data-test='table-paging-counter']");
+  private final SelenideElement paginationBackButton = $("button[data-test='table-paging-prev']");
+  private final SelenideElement paginationNextButton = $("button[data-test='table-paging-next']");
+
+  private final SelenideElement sortingArrow = $("button[data-test='table-column-sorting']");
+
+  //'Administrators' list columns
+  private final SelenideElement administratorColumn = $("th[data-test='administrator-title-column']");
+  private final SelenideElement emailAddressColumn = $("th[data-test='email-address-column']");
+  private final SelenideElement dateCreatedColumn = $("th[data-test='date-created-column']");
+  private final SelenideElement ownerColumn = $("th[data-test='owner-column']");
+
+
+  //Administrator rows
+  private final ElementsCollection administratorItemRow = $$("tr[data-test='administrator-item']");
+  private final ElementsCollection administratorRow = $$("tr[data-test='administrator-item-title']");
+  private final ElementsCollection emailAddressRow = $$("tr[data-test='administrator-item-email']");
+  private final ElementsCollection dateCreatedRow = $$("tr[data-test='administrator-item-creation-date']");
+  private final ElementsCollection ownerRow = $$("tr[data-test='administrator-item-creator']");
+
+  private final ElementsCollection threeDotMenuEventRow = $$("tr[data-test='administrator-item-menu-button']");
+
+  private final SelenideElement noAdministratorsSearchIcon = $("div[data-test='no-found-administrators-icon']");
+  private final SelenideElement noAdministratorsSearchText = $("div[data-test='no-found-administrators-label']");
 }
--- src/main/java/com/practis/web/selenide/page/admin/navigation/CompanyPage.java
+++ src/main/java/com/practis/web/selenide/page/admin/navigation/CompanyPage.java
@@ -1,7 +1,9 @@
 package com.practis.web.selenide.page.admin.navigation;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
 
@@ -12,4 +14,31 @@ public class CompanyPage {
       $("div[data-test='company-accounts-page-page-subtitle']");
   private final SelenideElement searchField = $("input[data-test='table-search-input']");
   private final SelenideElement searchFieldIcon = $("div[data-test='table-search-input-icon']");
+
+  private final SelenideElement updatedTimestampText = $("span[data-test='table-timestamp-label']");
+  private final SelenideElement updatedTimestampButton = $("button[data-test='table-timestamp-refresh']");
+
+  private final SelenideElement paginationCounterText = $("div[data-test='table-paging-counter']");
+  private final SelenideElement paginationBackButton = $("button[data-test='table-paging-prev']");
+  private final SelenideElement paginationNextButton = $("button[data-test='table-paging-next']");
+
+  private final SelenideElement sortingArrow = $("button[data-test='table-column-sorting']");
+
+  //'Company Accounts' list columns
+  private final SelenideElement companyColumn = $("th[data-test='company-title-column']");
+  private final SelenideElement companyOwnerColumn = $("th[data-test='company-owner-column']");
+  private final SelenideElement dateActivatedColumn = $("th[data-test='date-activated-column']");
+  private final SelenideElement ownerColumn = $("th[data-test='owner-column']");
+
+  //Company rows
+  private final ElementsCollection companyItemRow = $$("tr[data-test='company-item']");
+  private final ElementsCollection companyRow = $$("tr[data-test='company-item-title']");
+  private final ElementsCollection companyOwnerRow = $$("tr[data-test='company-item-creator']");
+  private final ElementsCollection dateActivatedRow = $$("tr[data-test='company-item-date-activated']");
+  private final ElementsCollection ownerRow = $$("tr[data-test='company-item-owner']");
+
+  private final ElementsCollection threeDotMenuEventRow = $$("tr[data-test='company-item-menu-button']");
+
+  private final SelenideElement noCompanySearchIcon = $("div[data-test='no-found-companies-icon']");
+  private final SelenideElement noCompanySearchText = $("div[data-test='no-found-companies-label']");
 }
