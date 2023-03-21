e44cb9fe2530fe226ac9dca99793c07dc814b3aa
Anastasiya Halubovich <riikkimarley@gmail.com>
2023-03-21 16:41:56 +0100

    limit memory size

--- src/main/java/com/practis/web/selenide/component/AssignUsersAndDueDatesModule.java
+++ src/main/java/com/practis/web/selenide/component/AssignUsersAndDueDatesModule.java
@@ -10,8 +10,9 @@ import lombok.Getter;
 @Getter
 public class AssignUsersAndDueDatesModule {
 
-    private final SelenideElement assignUsersAndDueDatesTitle = $(".sc-kJtljt.AgCPn");
-    private final SelenideElement practisSetSubtitle = $(".sc-frCTnc.iBDFas");
+    private final SelenideElement assignUsersAndDueDatesTitle =
+            $("div[data-test='assign-users-title']");
+    private final SelenideElement practisSetSubtitle = $("div[data-test='practis-set-name']");
 
     private final SelenideElement updatedTimestampText =
             $("span[data-test='table-timestamp-label']");
@@ -23,33 +24,36 @@ public class AssignUsersAndDueDatesModule {
     private final ElementsCollection searchFields = $$("input[data-test='table-search-input']");
     private final SelenideElement searchFieldCrossButton =
             $("div[data-test='practisset-searchbox-field-clear']");
-    private final SelenideElement searchFieldXButton = $(".sc-pVTma.iwyvnt");
-    private final SelenideElement filtersButton = $(".sc-kITQso.bMXjAm");
+    private final SelenideElement searchFieldXButton =
+            $("div[data-test='assign-users-search-clear']");
+    private final SelenideElement filtersButton =
+            $("button[data-test='assign-users-filters-button']");
 
     // Users table
     // columns
     private final SelenideElement itemsCounterText = $("p[data-test='table-selected-item']");
-    private final SelenideElement selectAllCheckbox = $(".sc-lhMhtZ.ftvXwI");
-    private final ElementsCollection columns = $$(".sc-cTgIxk.ckCjnW");
-    private final ElementsCollection dueDateColumn = $$(".sc-cTgIxk.fesHYS");
+    private final SelenideElement selectAllCheckbox =
+            $("div[data-test='assign-users-master-checkbox-input-view']");
+    private final SelenideElement userColumn = $("th[data-test='users-column']");
+    private final SelenideElement dueDateColumn = $("th[data-test='due-date-column']");
+    private final SelenideElement registeredOnColumn =
+            $("th[data-test='registration-date-column-text']");
     // rows
     private final ElementsCollection userRow = $$("tr[data-test='table-row']");
-    private final ElementsCollection userCheckbox = $$(".sc-lhMhtZ.ftvXwI");
+    private final ElementsCollection userCheckbox = $$("div[data-test='user-item-checkbox-view']");
     private final ElementsCollection userLabelRow = $$("div[data-test='table-labels']");
-    private final ElementsCollection userNameRow = $$(".sc-bcGzrp.hdFzGI");
-    private final ElementsCollection dueDateRow = $$(".sc-LQrcB.hsiNpg");
-    private final ElementsCollection registeredOnRow = $$(".sc-bDONLa.gbtPTg.jss8");
-    private final ElementsCollection threeDotButton = $$(".sc-hcZayR.ljVdd");
+    private final ElementsCollection userNameRow = $$("div[data-test='user-item-name']");
+    private final ElementsCollection dueDateRow = $$("span[data-test='user-item-due-date']");
+    private final ElementsCollection registeredOnRow =
+            $$("span[data-test='user-item-registration-date']");
+    private final ElementsCollection threeDotButton =
+            $$("div[data-test='due-date-actions-button']");
 
     private final SelenideElement noUserIcon = $(".sc-jlObCs.heRKBi");
     private final SelenideElement noUserText = $(".sc-NqzHV.heoJmO");
-    private final SelenideElement noUserFoundIcon =
-            $("div[data-test='practisset-searchbox-empty-result-icon']");
-    private final SelenideElement noUserFoundText =
-            $("div[data-test='practisset-searchbox-empty-result-label']");
 
-    private final SelenideElement noUsersFoundIcon = $(".sc-fkqjzy.gTwUsI");
-    private final SelenideElement noUsersFoundText = $(".sc-gdvdet.chqfSt");
+    private final SelenideElement noUsersFoundIcon = $("div[data-test='no-users-found-icon']");
+    private final SelenideElement noUsersFoundText = $("div[data-test='no-users-found-label']");
 
     private final SelenideElement assignSelectedUsersButton =
             $(".sc-caiKgP.hPyIrA.undefined.primary");
