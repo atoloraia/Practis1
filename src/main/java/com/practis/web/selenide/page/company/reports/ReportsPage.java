c327bdc5dcd52bd9e815a50855276fe4eb32a1c4
Anastasiya Halubovich <riikkimarley@gmail.com>
2023-04-14 13:22:49 +0200

    test_refactoring_12_04_2023

--- src/main/java/com/practis/web/selenide/page/company/reports/ReportsPage.java
+++ src/main/java/com/practis/web/selenide/page/company/reports/ReportsPage.java
@@ -10,25 +10,23 @@ import lombok.Getter;
 @Getter
 public class ReportsPage {
 
-    private final SelenideElement reportsTitle = $("div[data-test='practis-set-page-title']");
+  private final SelenideElement reportsTitle =
+      $("div[data-test='practis-set-page-title']");
 
-    private final SelenideElement practisSetSummaryIcon =
-            $("span[data-test='practis-set-not-published-yet']");
-    private final SelenideElement practisSetSummaryTitle =
-            $("input[data-test='practis-set-title']");
-    private final SelenideElement practisSetSummaryText = $("input[data-test='practis-set-title']");
+  private final SelenideElement practisSetSummaryIcon =
+      $("span[data-test='practis-set-not-published-yet']");
+  private final SelenideElement practisSetSummaryTitle = $("input[data-test='practis-set-title']");
+  private final SelenideElement practisSetSummaryText = $("input[data-test='practis-set-title']");
 
-    private final SelenideElement userActivityIcon =
-            $("span[data-test='practis-set-not-published-yet']");
-    private final SelenideElement userActivityTitle = $("input[data-test='practis-set-title']");
-    private final SelenideElement userActivityText = $("input[data-test='practis-set-title']");
+  private final SelenideElement userActivityIcon =
+      $("span[data-test='practis-set-not-published-yet']");
+  private final SelenideElement userActivityTitle = $("input[data-test='practis-set-title']");
+  private final SelenideElement userActivityText = $("input[data-test='practis-set-title']");
 
-    private final SelenideElement teamLeaderSummaryIcon =
-            $("span[data-test='practis-set-not-published-yet']");
-    private final SelenideElement teamLeaderSummaryTitle =
-            $("input[data-test='practis-set-title']");
-    private final SelenideElement teamLeaderSummaryText = $("input[data-test='practis-set-title']");
+  private final SelenideElement teamLeaderSummaryIcon =
+      $("span[data-test='practis-set-not-published-yet']");
+  private final SelenideElement teamLeaderSummaryTitle = $("input[data-test='practis-set-title']");
+  private final SelenideElement teamLeaderSummaryText = $("input[data-test='practis-set-title']");
 
-    private final ElementsCollection excelText =
-            $$("button[data-test='save-practis-set-as-draft']");
+  private final ElementsCollection excelText = $$("button[data-test='save-practis-set-as-draft']");
 }
